568be6798bb19664322bdf381e05f16bca9f1098
vmanuelpm <vmanuelpm97@hotmail.com>
2020-08-29 13:13:24 -0500

    añadido proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LateralComponent } from './components/lateral/lateral.component';
+import { CentroSuperiorComponent } from './components/centro-superior/centro-superior.component';
+import { CentroInferiorComponent } from './components/centro-inferior/centro-inferior.component';
+
+import { NgxStarsModule } from 'ngx-stars';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LateralComponent,
+    CentroSuperiorComponent,
+    CentroInferiorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxStarsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
